981feb1f334821dc1854fc1116a9befab84f5340
Ana Azevedo <ana.fazevedo@b2wdigital.com>
2018-09-17 16:28:56 -0300

    feat: Cria componente modal

--- source/components/modal/modal-component.js
+++ source/components/modal/modal-component.js
@@ -1,15 +1,48 @@
 import React, { PureComponent } from 'react';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 import CSSModules from 'react-css-modules';
+import classNames from 'classnames';
+
+import Icon from '../icon';
 
 import styles from './modal.styl';
 
 class Modal extends PureComponent {
-  static propTypes = {};
-  static defaultProps = {};
+  constructor(props) {
+    super(props);
+    this.closeModal = this.closeModal.bind(this);
+  }
+
+  static propTypes = {
+    children: PropTypes.element.isRequired,
+    open: PropTypes.bool.isRequired,
+    showClose: PropTypes.bool,
+    closeModal: PropTypes.func,
+  };
+
+  static defaultProps = {
+    showClose: true,
+  };
+
+  closeModal() {
+    this.props.closeModal && this.props.closeModal();
+  }
 
   render() {
-    return <div>Modal</div>;
+    const { open, children, showClose, className, ...rest } = this.props;
+    const fullClassName = classNames(className, styles.modal, {
+      [styles.isOpen]: open,
+    });
+
+    return (
+      <div className={classNames(styles.wrap, { [styles.isOpen]: open })}>
+        <div className={fullClassName} {...rest}>
+          {showClose && <Icon onClick={this.closeModal} className={styles.close} size="20" name="close" />}
+          <div className={styles.content}>{children}</div>
+        </div>
+        <div onClick={this.closeModal} className={classNames(styles.overlay, { [styles.isOpen]: open })} />
+      </div>
+    );
   }
 }
 
--- source/components/modal/modal.stories.js
+++ source/components/modal/modal.stories.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { withKnobs } from '@storybook/addon-knobs';
+import { withState } from '@dump247/storybook-state';
+
+import Button from '../button';
 
 import Modal from './modal-component';
 
@@ -8,4 +11,22 @@ const stories = storiesOf('Modal', module);
 
 stories.addDecorator(withKnobs);
 
-stories.addWithInfo('Normal', () => <Modal />);
+stories.addWithInfo('Normal', withState({ open: false })(({ store }) => {
+  const openModal = () => {
+    store.set({ open: true });
+  };
+
+  const closeModal = () => {
+    store.set({ open: false });
+  };
+
+  return <div>
+    <Button onClick={openModal}>Abrir Modal</Button>
+    <Modal open={store.state.open} closeModal={closeModal}>
+      <div>
+        <p>Olar</p>
+        <Button onClick={closeModal}>Fechar</Button>
+      </div>
+    </Modal>
+  </div>
+}));
