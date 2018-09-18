f8a48f6c0dd190af31625deff39c30231c0b3f4c
Ana Azevedo <ana.fazevedo@b2wdigital.com>
2018-09-18 19:16:50 -0300

    feat: Componente inicial de tabela

--- source/components/table/table-component.js
+++ source/components/table/table-component.js
@@ -5,55 +5,126 @@ import classNames from 'classnames';
 
 import styles from './table.styl';
 
+import { uniqueId } from '../../helpers';
+
 class Table extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: props.data,
+    };
+  }
+
   static propTypes = {
-    schema: PropTypes.object,
-    data: PropTypes.array,
-    head: PropTypes.array,
+    type: PropTypes.oneOf(['auto', 'fixed']),
+    schema: PropTypes.objectOf(
+      PropTypes.shape({
+        title: PropTypes.string,
+        className: PropTypes.string,
+        headClassName: PropTypes.string,
+        width: PropTypes.string,
+        renderHead: PropTypes.func,
+        render: PropTypes.func,
+      })
+    ).isRequired,
+    data: PropTypes.oneOf([PropTypes.array, PropTypes.arrayOf(PropTypes.object)]),
+    notFoundMessage: PropTypes.oneOf([PropTypes.string, PropTypes.element]),
+    loadingMessage: PropTypes.oneOf([PropTypes.string, PropTypes.element]),
+    tableClassName: PropTypes.string,
+    scrollY: PropTypes.bool,
+    scrollX: PropTypes.bool,
+    width: PropTypes.string,
+    height: PropTypes.string,
   };
 
-  static defaultProps = {};
+  static defaultProps = {
+    type: 'auto',
+    notFoundMessage: 'Nenhum resultado encontrado :(',
+    loadingMessage: 'Loading...',
+  };
 
-  renderRow(schema, data) {
-    return data.map(row => {
-      return (
-        <tr className={styles.row}>
-          {Object.keys(schema).map(name => {
-            const currentValue = row[name];
-            const currentSchema = schema[name];
-            return (
-              <td className={classNames(styles.cell, currentSchema.className)}>
-                {currentSchema.render(currentValue)}
-              </td>
-            );
-          })}
-        </tr>
-      );
-    });
+  renderHeadRow(schema) {
+    return (
+      <tr className={styles.rowHead}>
+        {Object.keys(schema).map(key => {
+          const currentSchema = schema[key];
+          const headClass = classNames(styles.cellHead, currentSchema.className);
+
+          return (
+            <th width={currentSchema.width} key={uniqueId()} className={headClass}>
+              {currentSchema.renderHead ? currentSchema.renderHead(currentSchema) : currentSchema.title}
+            </th>
+          );
+        })}
+      </tr>
+    );
+  }
+
+  renderRow(data, schema) {
+    return data.map(infoRow => (
+      <tr key={uniqueId()} className={styles.row}>
+        {Object.keys(schema).map(key => {
+          const currentSchema = schema[key];
+          return (
+            <td
+              width={currentSchema.width}
+              key={uniqueId()}
+              className={classNames(styles.cell, currentSchema.className)}
+            >
+              {currentSchema.render ? currentSchema.render(infoRow) : infoRow[key]}
+            </td>
+          );
+        })}
+      </tr>
+    ));
+  }
+
+  renderEmptyResults(message, schema) {
+    return (
+      <tr>
+        <td colSpan={schema.length} className={styles.cell}>
+          {message}
+        </td>
+      </tr>
+    );
+  }
+
+  verifyResults(data, schema) {
+    const { loadingMessage, notFoundMessage } = this.props;
+
+    if (!Array.isArray(data)) {
+      return this.renderEmptyResults(loadingMessage, schema);
+    } else if (data.length === 0) {
+      return this.renderEmptyResults(notFoundMessage, schema);
+    } else {
+      return this.renderRow(data, schema);
+    }
   }
 
   render() {
-    const { schema, data } = this.props;
+    const { data, schema, type, tableClassName, className, scrollY, scrollX, width, height, ...rest } = this.props;
+    const tableClass = classNames(tableClassName, styles.table, {
+      [styles.isFixed]: type === 'fixed',
+    });
+    const wrapClass = classNames(styles.wrap, className, {
+      [styles.isScrollY]: scrollY,
+      [styles.isScrollX]: scrollX,
+    });
+
+    const wrapSizes = {
+      width: width || 'auto',
+      height: height || 'auto',
+    };
 
     return (
-      <div className={styles.content}>
-        <table className={styles.table}>
+      <div style={wrapSizes} className={wrapClass} {...rest}>
+        <table className={tableClass}>
           <thead className={styles.tableHead}>
-            <tr className={styles.rowHead}>
-              {Object.keys(schema).map(name => {
-                const currentSchema = schema[name];
-                const headClass = classNames(styles.cellHead, currentSchema.className, currentSchema.headClassName);
-                return (
-                  <th key={currentSchema.title} className={headClass}>
-                    {currentSchema.render(currentSchema.title)}
-                  </th>
-                );
-              })}
-            </tr>
+            {this.renderHeadRow(schema)}
           </thead>
 
           <tbody className={styles.tableBody}>
-            {this.renderRow(schema, data)}
+            {this.verifyResults(data, schema)}
           </tbody>
         </table>
       </div>
--- source/components/table/table.stories.js
+++ source/components/table/table.stories.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { withKnobs } from '@storybook/addon-knobs';
+import { withState } from '@dump247/storybook-state';
 
 import Table from './table-component';
+import Panel from '../panel';
+import Button from '../button';
 
 const stories = storiesOf('Table', module);
 
@@ -10,14 +13,22 @@ stories.addDecorator(withKnobs);
 
 const simpledata = [
   {
-    name: 'Fulano',
-    surname: 'da silva',
-    profession: 'Cameraman do Cabo Daciolo',
+    name: { first: 'Fulano', last: 'da Silva' },
+    gender: 'male',
+    email: 'fulano123@teste.com',
+    dob: { age: '45' },
   },
   {
-    name: 'Lucrécia',
-    surname: 'da Silva e Souza',
-    profession: '',
+    name: { first: 'Fulano', last: 'da Silva' },
+    gender: 'male',
+    email: 'fulano123@teste.com',
+    dob: { age: '45' },
+  },
+  {
+    name: { first: 'Fulano', last: 'da Silva' },
+    gender: 'male',
+    email: 'fulano123@teste.com',
+    dob: { age: '45' },
   }
 ];
 
@@ -25,21 +36,62 @@ const schema = {
   name: {
     title: 'Nome',
     headClassName: null,
+    width: '100px',
     className: null,
-    render: data => data,
+    render: info => info.name.first,
   },
   surname: {
     title: 'Sobrenome',
+    headClassName: null,
     className: null,
-    render: data => data,
+    render: info => info.name.last,
   },
-  profession: {
-    title: 'Profissão',
+  gender: {
+    title: 'Gênero',
     className: null,
-    render: data => data,
+  },
+  email: {
+    title: 'E-mail',
+    className: null,
+  },
+  oi: {
+    title: 'Diga Oi',
+    className: null,
+    headClassName: null,
+    renderHead: () => <Button onClick={() => alert('Olá!')}>Diga oi para todos!</Button>,
+    render: info => <Button onClick={() => alert(`Tenho ${info.dob.age} anos!`)}>Adivinhe minha idade ;)</Button>,
   },
 };
 
 stories.addWithInfo('Normal', () => {
-  return <Table schema={schema} data={simpledata} />
+  return (
+    <Table schema={schema} data={simpledata} />
+  );
 });
+
+stories.addWithInfo('Fixed', () => {
+  return (
+    <Table type="fixed" schema={schema} data={simpledata} />
+  );
+});
+
+stories.addWithInfo('Scroll', () => {
+  return (
+    <Table scrollY scrollX width="500px" height="150px" schema={schema} data={simpledata} />
+  );
+});
+
+
+stories.addWithInfo('With Data and Panel', withState({ data: undefined })(({ store }) => {
+  fetch('https://randomuser.me/api/?results=10')
+    .then(res => res.json())
+    .then(res => {
+      !store.state.data && store.set({ data: res.results });
+    });
+
+  return (
+    <Panel>
+      <Table schema={schema} data={store.state.data} />
+    </Panel>
+  );
+}));
