01fee398823dc1a6476ac2cc4e8c3d9c8c14adcd
Ana Azevedo <ana.fazevedo@b2wdigital.com>
2018-09-19 18:24:47 -0300

    fix: Ajusta teste da tabela

--- source/components/table/table.spec.js
+++ source/components/table/table.spec.js
@@ -2,13 +2,28 @@ import '../../../internals/test/helper';
 
 import Table from './table-component';
 
+const simpledata = [
+  {
+    name: 'Teste',
+  },
+];
+
+const schema = {
+  name: {
+    title: 'Nome',
+    headClassName: null,
+    width: '100px',
+    className: null,
+  },
+};
+
 /** @test {Table} */
 describe('Table component', () => {
 /** @test {Table#render} */
   describe('#render', () => {
     it('render correctly', () => {
       const wrapper = shallow(
-        <Table />
+        <Table schema={schema} data={simpledata} />
       );
       expect(wrapper.length).toEqual(1);
     });
