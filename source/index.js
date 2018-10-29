f295532491f05d33831f344410798c3621443a8b
Ana Azevedo <ana.fazevedo@b2wdigital.com>
2018-10-29 17:05:54 -0300

    feat Adiciona erro no tablepanel

--- source/index.js
+++ source/index.js
@@ -29,6 +29,7 @@ import Slider from './components/slider';
 import LinkGenerator from './components/link-generator';
 import Alert from './components/alert';
 import LineGraph from './components/line-graph';
+import Error from './components/error';
 
 // HELPERS
 import { fieldsValidation } from './helpers/validation';
@@ -73,4 +74,5 @@ export {
   Slider,
   Alert,
   LineGraph,
+  Error,
 };
