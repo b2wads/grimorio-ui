15ce64e6928da38a4aa8054ff87243c71f391cc5
Deborah Miranda Casanova Costa <LAB2W\deborah.costa@brrjobitanl1081.la.ad.b2w>
2019-06-06 19:12:32 -0300

    feat: tabela fixa header, footer e conteúdo a esquerda

---
+++ source/components/table-fixed/table-fixed-component.js
@@ -0,0 +1,66 @@
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+import CSSModules from 'react-css-modules';
+import Table from '../table';
+
+import styles from './table-fixed.styl';
+
+class TableFixed extends PureComponent {
+  static propTypes = {
+    schemaLeft: PropTypes.objectOf(
+      PropTypes.shape({
+        title: PropTypes.string,
+        className: PropTypes.string,
+        headClassName: PropTypes.string,
+        width: PropTypes.string,
+        renderHead: PropTypes.func,
+        render: PropTypes.func,
+      })
+    ).isRequired,
+    schemaRight: PropTypes.objectOf(
+      PropTypes.shape({
+        title: PropTypes.string,
+        className: PropTypes.string,
+        headClassName: PropTypes.string,
+        width: PropTypes.string,
+        renderHead: PropTypes.func,
+        render: PropTypes.func,
+      })
+    ).isRequired,
+  };
+  static defaultProps = {};
+
+  render() {
+    const { schemaLeft, data, schemaRight, width, height, dataFooterRight, dataFooterLeft } = this.props;
+    const wrapSizes = {
+      maxWidth: width || 'auto',
+      maxHeight: height || 'auto',
+    };
+    return (
+      <div className={styles.wrap} style={wrapSizes}>
+        <div className={styles.sticky}>
+          <Table
+            className={styles.table}
+            type="panel"
+            isSticky
+            schema={schemaLeft}
+            data={data}
+            dataFooter={dataFooterLeft}
+          />
+        </div>
+        <div>
+          <Table
+            className={styles.table}
+            type="panel"
+            isSticky
+            schema={schemaRight}
+            data={data}
+            dataFooter={dataFooterRight}
+          />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default CSSModules(TableFixed, styles);
