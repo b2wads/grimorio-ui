285c91a439063e9fe39e0a7461872e8783a923b8
Deborah Miranda Casanova Costa <deborah.costa@b2wdigital.com>
2019-06-25 14:22:00 -0300

    test: adicionando testes do componente de pager

--- source/components/pager/pages.spec.js
+++ source/components/pager/pages.spec.js
@@ -1,24 +1,105 @@
 import '../../../internals/test/helper';
 
 import Pager from './pager-component';
+import Select from '../select';
 
 /** @test {Pager} */
 describe('Pager component', () => {
 /** @test {Pager#render} */
   describe('#render', () => {
-    it('render correctly', () => {
-      const wrapper = mount(
-        <Pager
-          perpage={10}
-          length={20}
-          onLimitChange={() => {}}
-          onClickPagination={() => {}}
-          limitList={15}
-          hasFirstLast
-          hasPagination
-        />
+    let wrapper;
+    const props = {
+      perpage: 10,
+      length: 20,
+      onLimitChange: jest.fn(),
+      onClickPagination: jest.fn(),
+      hasFirstLast: false,
+      hasPagination: false,
+      count: 100,
+      limit: 15,
+      offset: 20,
+      limitList: [10, 15, 50],
+    }
+    beforeEach(() => {
+      wrapper = shallow(
+        <Pager {...props} />
       );
-      expect(wrapper.length).toEqual(1);
+
+      jest.spyOn(props, 'onLimitChange');
+      jest.spyOn(props, 'onClickPagination');
+    })
+    it('render correctly', () => {
+      expect(wrapper).toMatchSnapshot();
+    });
+
+     it('render correctly with hasFirstLast', () => {
+      wrapper.setProps({ hasFirstLast: true });
+      expect(wrapper).toMatchSnapshot();
+    });
+    it('onClick pagination first button', () => {
+      wrapper.setProps({ hasFirstLast: true });
+      wrapper.find('.nav .btn').at(0).simulate('click');
+      expect(props.onClickPagination).toBeCalledWith('first');
+    });  
+
+    it('onClick pagination prev button', () => {
+      wrapper.setProps({ hasFirstLast: true });
+      wrapper.find('.nav .btn').at(1).simulate('click');
+      expect(props.onClickPagination).toBeCalledWith('prev');
+    });
+
+    it('onClick pagination next button', () => {
+      wrapper.setProps({ hasFirstLast: true });
+      wrapper.find('.nav .btn').at(2).simulate('click');
+      expect(props.onClickPagination).toBeCalledWith('next');
+    });
+
+    it('onClick pagination last button', () => {
+      wrapper.setProps({ hasFirstLast: true });
+      wrapper.find('.nav .btn').at(3).simulate('click');
+      expect(props.onClickPagination).toBeCalledWith('last');
+    });
+
+    it('render correctly with hasPagination', () => {
+      wrapper.setProps({ hasPagination: true });
+      expect(wrapper).toMatchSnapshot();
+    });
+
+    it('onClick pagination number button', () => {
+      wrapper.setProps({ hasPagination: true });
+      wrapper.find('.pagesWrap .btn').at(1).simulate('click');
+      expect(props.onClickPagination).toBeCalledWith('number', 2);
+    });
+
+    it('return null when count is undefined', () => {
+      wrapper.setProps({ count: undefined });
+      expect(wrapper.html()).toEqual(null);
+    });
+
+    it('return null when perpage is undefined', () => {
+      wrapper.setProps({ perpage: undefined });
+      expect(wrapper.html()).toEqual(null);
+    });
+    
+    it('should call function when changes perpage select', () => {
+      const obj = { value: 10 };
+      wrapper.find('.perpage').find(Select).prop('onSelect')(obj);
+      expect(props.onLimitChange).toHaveBeenCalledWith(obj.value);
+    });
+
+    it('change start and end pages when currentPage <= 6', () => {
+      wrapper.setProps({ offset: 60, hasPagination: true, count: 500 });
+      expect(wrapper).toMatchSnapshot();
+    });
+
+    it('change start and end pages when currentPage + 4 >= maxPages', () => {
+      wrapper.setProps({ offset: 300, hasPagination: true, count: 300 });
+      expect(wrapper).toMatchSnapshot();
+    });
+
+    it('change start and end pages when is a default value', () => {
+      wrapper.setProps({ offset: 100, hasPagination: true, count: 500 });
+      expect(wrapper).toMatchSnapshot();
     });
   });
 });
