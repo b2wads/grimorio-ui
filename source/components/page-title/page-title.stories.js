7e92f37874f03004bc5148bd644bdcfcd61c87f3
Lucas Domingues <lucasdomi@gmail.com>
2019-07-16 16:15:39 -0300

    refact: removendo flex-direction default em titleWrap

--- source/components/page-title/page-title.stories.js
+++ source/components/page-title/page-title.stories.js
@@ -9,6 +9,6 @@ const stories = storiesOf('PageTitle', module);
 
 stories.addDecorator(withKnobs);
 
-stories.add('Normal', () => <PageTitle title="Dashboard" sideComponent={<Button>Hello</Button>} />);
+stories.add('Normal', () => <PageTitle isMobile={false} title="Dashboard" sideComponent={<Button>Hello</Button>} />);
 
 stories.add('Mobile', () => <PageTitle isMobile title="Dashboard" sideComponent={<Button>Hello</Button>} />);
