7db9db103a0a9d11742438fabf019c1de92a94ec
Lucas Domingues <lucasdomi@gmail.com>
2019-07-19 15:14:51 -0300

    test: incrementa logica no teste do steps

--- source/components/steps/steps.spec.js
+++ source/components/steps/steps.spec.js
@@ -2,12 +2,20 @@ import '../../../internals/test/helper';
 
 import Steps from './steps-component';
 
+const pageData = [
+  { name: 'pass1', title: 'Criação de Campanha' },
+  { name: 'pass2', title: 'Criação de grupo de anúncio' },
+  { name: 'pass3', title: 'Adicionar anúncios' },
+  { name: 'finish', title: 'Finalização' },
+];
+
+let value = 0;
 /** @test {Slider} */
 describe('Slider component', () => {
   /** @test {Slider#render} */
   describe('#render', () => {
     it('render correctly', () => {
-      const wrapper = shallow(<Steps />);
+      const wrapper = shallow(<Steps data={pageData} current={pageData[value].name} value={value} />);
       expect(wrapper.length).toEqual(1);
     });
   });
