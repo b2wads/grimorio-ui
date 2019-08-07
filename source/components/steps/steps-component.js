1f817d5f45c17dbd498fc974a21a80ef1b168fa5
Lucas Domingues <lucasdomi@gmail.com>
2019-08-07 15:34:18 -0300

    refact: removendo fragment antes do panel

--- source/components/steps/steps-component.js
+++ source/components/steps/steps-component.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import cx from 'classnames';
 import styles from './steps.styl';
@@ -7,26 +7,24 @@ import Panel from '../panel/index';
 
 const Steps = ({ data, current }) => {
   return (
-    <Fragment>
-      <Panel>
-        <div className={styles.steps}>
-          {data.map((step, index) => (
-            <div
-              className={cx(styles.stepsHolder, {
-                [styles.stepsHolderActive]: step.isComplete,
-                [styles.stepsCurrent]: step.name === current,
-              })}
-            >
-              <span className={cx(styles.stepsNumber, { [styles.currentNumber]: step.name === current })}>
-                {step.isComplete ? <Icon name="check" className={styles.icon} size={16} /> : index + 1}
-              </span>
-              <span>{step.title}</span>
-              {index + 1 < data.length && <div className={styles.stepsHolderLine} />}
-            </div>
-          ))}
-        </div>
-      </Panel>
-    </Fragment>
+    <Panel>
+      <div className={styles.steps}>
+        {data.map((step, index) => (
+          <div
+            className={cx(styles.stepsHolder, {
+              [styles.stepsHolderActive]: step.isComplete,
+              [styles.stepsCurrent]: step.name === current,
+            })}
+          >
+            <span className={cx(styles.stepsNumber, { [styles.currentNumber]: step.name === current })}>
+              {step.isComplete ? <Icon name="check" className={styles.icon} size={16} /> : index + 1}
+            </span>
+            <span>{step.title}</span>
+            {index + 1 < data.length && <div className={styles.stepsHolderLine} />}
+          </div>
+        ))}
+      </div>
+    </Panel>
   );
 };
 
