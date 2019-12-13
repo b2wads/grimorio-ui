553c0be3369befe1b234f692f893065623f1cdd3
Lucas Domingues <lucasdomi@gmail.com>
2019-12-13 17:45:18 -0300

    feat: troca nome do botao de value para ativo

--- source/components/toggle-button/toggle-button-component.js
+++ source/components/toggle-button/toggle-button-component.js
@@ -3,14 +3,14 @@ import cx from 'classnames';
 
 import styles from './toggle-button.styl';
 
-const ToggleBool = ({ value, onClick }) => {
+const ToggleBool = ({ active, onClick }) => {
   return (
     <div className={styles.toggleBool}>
-      <span onClick={() => onClick(true)} className={cx(styles.toggleBoolLeft, { [styles.active]: value })}>
+      <span onClick={() => onClick(true)} className={cx(styles.toggleBoolLeft, { [styles.active]: active })}>
         Sim
       </span>
 
-      <span onClick={() => onClick(false)} className={cx(styles.toggleBoolRight, { [styles.active]: !value })}>
+      <span onClick={() => onClick(false)} className={cx(styles.toggleBoolRight, { [styles.active]: !active })}>
         Não
       </span>
     </div>
