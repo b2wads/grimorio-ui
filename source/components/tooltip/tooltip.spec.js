90bb5deb00dfb5088567c8470003df7094073cbe
Deborah Miranda Casanova Costa <deborah.costa@b2wdigital.com>
2020-02-04 16:38:29 -0300

    feat: teste do componente de tooltip

--- source/components/tooltip/tooltip.spec.js
+++ source/components/tooltip/tooltip.spec.js
@@ -1,16 +1,40 @@
 import '../../../internals/test/helper';
 
 import Tooltip from './tooltip-component';
+import Icon from '../icon';
 
 /** @test {Tooltip} */
 describe('Tooltip component', () => {
-/** @test {Tooltip#render} */
-  describe('#render', () => {
-    it('render correctly', () => {
-      const wrapper = shallow(
-        <Tooltip message="hello" />
-      );
-      expect(wrapper.length).toEqual(1);
-    });
+  /** @test {Tooltip#render} */
+
+  let wrapper
+  const emptyProps = {
+    message: null,
+    children: null
+  }
+
+  beforeAll(() => {
+    wrapper = shallow(
+      <Tooltip />
+    );
+  })
+
+  it('should render with default props', () => {
+    wrapper.setProps({ message: 'hello'})
+    expect(wrapper.debug()).toMatchSnapshot();
+  });
+
+  it('should render with children value', () => {
+    const childrenValue = 'texto do tooltip'
+    wrapper.setProps({ ...emptyProps , children: childrenValue})
+    expect(wrapper.find('span').first().text()).toEqual(childrenValue);
+  });
+
+  it('should render icon with name and size', () => {
+    const icon = 'iconEye'
+    const size = 5
+    wrapper.setProps({ ...emptyProps, icon, size })
+    expect(wrapper.find(Icon).props().name).toEqual(icon);
+    expect(wrapper.find(Icon).props().size).toEqual(size);
   });
 });
