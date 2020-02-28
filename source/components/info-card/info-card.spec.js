5f37b9cf9b6f56f71dadedbebd507e3dbfe4237d
Deborah Miranda Casanova Costa <deborah.costa@b2wdigital.com>
2020-02-28 12:12:38 -0300

    test: teste do componente de inforCard

--- source/components/info-card/info-card.spec.js
+++ source/components/info-card/info-card.spec.js
@@ -2,16 +2,61 @@ import '../../../internals/test/helper';
 import React from 'react';
 
 import InfoCard from './info-card-component';
+import Error from '../error';
 
 /** @test {InfoCard} */
 describe('InfoCard component', () => {
-/** @test {InfoCard#render} */
-  describe('#render', () => {
-    it('render correctly', () => {
-      const wrapper = shallow(
-        <InfoCard />
-      );
-      expect(wrapper.length).toEqual(1);
+  /** @test {InfoCard#render} */
+
+  const emptyProps = {
+    error: undefined,
+    type: undefined,
+    tagline: undefined,
+    value: undefined,
+    onErrorClick: undefined,
+    errorMessage: undefined,
+    errorBtnText: undefined,
+  }
+
+  let wrapper
+
+  beforeAll(() => {
+    wrapper = shallow(
+      <InfoCard {...emptyProps} />
+    );
+  })
+
+  describe('when error card', () => {
+    const props = {
+      onErrorClick: jest.fn()
+    }
+    const spy = jest.spyOn(props, 'onErrorClick')
+    
+    it('should render correct', () => {
+      wrapper.setProps({
+        ...emptyProps,
+        error: true,
+        onErrorClick: props.onErrorClick,
+        errorMessage: 'mensagem de erro',
+        errorBtnText: 'botão do erro'
+      })
+      expect(wrapper.html()).toMatchSnapshot();
+    })
+
+    it('should call onErrorClick when click on button error', () => {
+
+      wrapper.find(Error).props().onErrorClick()
+      expect(spy).toHaveBeenCalled();
+    })
+  })
+
+    it('should render card type money', () => {
+      wrapper.setProps({ ...emptyProps, type: 'money', value: 20 })
+      expect(wrapper.html()).toMatchSnapshot();
     });
-  });
+
+    it('should render card with tagline', () => {
+      wrapper.setProps({ ...emptyProps, tagline: 'fevereiro' })
+      expect(wrapper.html()).toMatchSnapshot();
+    });
 });
