cf8dc865ea622ca6150ba8785cf9521ff678d564
Ana Azevedo <ana.fazevedo@b2wdigital.com>
2020-04-06 17:23:22 -0300

    feat(buttonUpload) Adiciona validação por dimensao

--- source/components/button-upload/button-upload.stories.js
+++ source/components/button-upload/button-upload.stories.js
@@ -5,21 +5,31 @@ import ButtonUpload from './index';
 
 const stories = storiesOf('Button Upload', module);
 
+const printRes = (data, list, error, size) => {
+  console.log('images: ', data, 'list:', list, 'error:', error, 'size', size);
+};
+
+stories.add('Normal', () => <ButtonUpload onChange={printRes} />);
+
+stories.add('As Div', () => <ButtonUpload as="div" onChange={printRes} />);
+
 stories.add(
-  'Normal',
+  'With image Dimensions',
   () => (
-    <div>
-      <ButtonUpload onChange={(data, list) => console.log('images: ', data, 'list:', list)} />
-    </div>
+    <ButtonUpload
+      allowedDimensions={['300x124', '1280x720']}
+      formatWhiteList={['.jpg', '.jpeg', '.png']}
+      btnText="Apenas Ibagens"
+      onChange={printRes}
+    />
   )
 );
-
 
 stories.add('With Limit', () =>
   <ButtonUpload
     btnText="Apenas 2 imagens"
     limit={2}
-    onChange={(data, list) => console.log('images: ', data, 'list:', list)}
+    onChange={printRes}
   />
 );
 
@@ -27,7 +37,7 @@ stories.add('With Extension Whitelist', () =>
   <ButtonUpload
     formatWhiteList={['.jpg', '.jpeg']}
     btnText="Apenas JPG e JPEG"
-    onChange={(data, list, error) => console.log('images: ', data, 'list:', list, 'error:', error)}
+    onChange={printRes}
   />
 );
 
@@ -35,6 +45,6 @@ stories.add('With MaxFileSize', () =>
   <ButtonUpload
     maxFileSize={100000}
     btnText="Até 100KB"
-    onChange={(data, list, error) => console.log('images: ', data, 'list:', list, 'error:', error)}
+    onChange={printRes}
   />
 );
