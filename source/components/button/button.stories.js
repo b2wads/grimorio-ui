7350fec5fb2500d0c7a2d5c587dfc1ed939165d3
Ana Azevedo <ana.fazevedo@b2wdigital.com>
2020-10-05 18:35:35 -0300

    feat Criando story em mdx

--- source/components/button/button.stories.js
+++ source/components/button/button.stories.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import Button from './index';
 
-export default {
-  title: 'Button',
-  component: Button,
-};
+// export default {
+//   title: 'Button',
+//   component: Button,
+// };
 
 export const Color = () => (
   <div>
